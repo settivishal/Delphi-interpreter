da855cdbde45b160666e71186673dd6eda83488b
settivishal <vishalsetti2000@gmail.com>
2025-04-06 18:10:02 -0400

    Added the implementation back

--- src/main/java/org/example/antlr/ExecuteVisitor.java
+++ src/main/java/org/example/antlr/ExecuteVisitor.java
@@ -479,5 +479,29 @@ public class ExecuteVisitor extends delphiBaseVisitor<Object>{
 
         return visitChildren(ctx);
     }
+
+    // Implement visitor for BREAK statement
+    @Override
+    public Object visitBreakStatement(delphiParser.BreakStatementContext ctx) {
+        if (inLoop) {
+            System.out.println("BREAK statement encountered");
+            throw new BreakException();
+        } else {
+            System.out.println("ERROR: BREAK statement outside of loop context");
+        }
+        return null;
+    }
+
+    // Implement visitor for CONTINUE statement
+    @Override
+    public Object visitContinueStatement(delphiParser.ContinueStatementContext ctx) {
+        if (inLoop) {
+            System.out.println("CONTINUE statement encountered");
+            throw new ContinueException();
+        } else {
+            System.out.println("ERROR: CONTINUE statement outside of loop context");
+        }
+        return null;
+    }
 }
 
