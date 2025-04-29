c8b26ab6f524300d976f6e9291147a48d05c64bf
settivishal <vishalsetti2000@gmail.com>
2025-04-29 12:42:58 -0400

    improved UI

--- src/main/java/org/example/antlr/LLVMCodeGenerator.java
+++ src/main/java/org/example/antlr/LLVMCodeGenerator.java
@@ -50,11 +50,6 @@ public class LLVMCodeGenerator extends delphiBaseVisitor<Object> {
         finalIR.add("; LLVM IR for Extended Pascal/Delphi");
         finalIR.add("target datalayout = \"e-m:e-p270:32:32-p271:32:32-p272:64:64-i64:64-f80:128-n8:16:32:64-S128\"");
         finalIR.add("target triple = \"wasm32-unknown-unknown\"");
-//        finalIR.add("declare noalias i8* @malloc(i64)");
-//        finalIR.add("declare void @free(i8*)");
-//        finalIR.add("declare void @writeln_i32(i32)");
-//        finalIR.add("declare void @writeln_str(i8*)");
-//        finalIR.add("");
 
         // 2. Add string literals next
         for (Map.Entry<String, String> entry : stringLiterals.entrySet()) {
